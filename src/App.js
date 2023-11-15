d7ebeb666ff44584b2fe07fc66c1011cf48cc828
Keyu Yao <keyu.yao@mail.mcgill.ca>
2023-11-14 23:49:58 -0500

    Add ingredient page
    static ingredient page, need to fix navigation bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from './components/Login';
-import Register from './components/Register';
-import Welcome from './components/Welcome';
-import Home from './components/Home';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Welcome from "./components/Welcome";
+import Home from "./components/Home";
+import Ingredients from "./components/Ingredients";
 
 function App() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Welcome />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/home' element={<Home />}/>
-        </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/ingredients" element={<Ingredients />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
